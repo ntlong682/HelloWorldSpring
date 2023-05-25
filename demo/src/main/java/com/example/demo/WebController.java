bcff9518950cc9897e18e9aea600fa431d4f9e76
LongNT <longnthe140162@fpt.edu.vn>
2023-05-25 16:47:39 +0700

    Update them

--- demo/src/main/java/com/example/demo/WebController.java
+++ demo/src/main/java/com/example/demo/WebController.java
@@ -1,11 +1,31 @@
 package com.example.demo;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	@GetMapping("hello")
+	public String hello(
+			//Request param "name" sẽ được gán  trí vào biến String
+			@RequestParam(name = "name", required = false, defaultValue = "") String 
+			name,
+			//Model là một obj của Spring Boot, được gắn vào mọi request
+			Model model) {
+		//Gắn vào model giá trị name nhận được
+		model.addAttribute("name",name);
+		return "hello";
 	}
 }
